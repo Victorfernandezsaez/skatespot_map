c0893ccf8de9fdcef00698d22a75b5376731f3ec
Victorfernandezsaez <v.ferna.saez@gmail.com>
2022-10-10 13:52:15 +0100

    newMarkers

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import { MapContainer, TileLayer, Popup, Marker} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 function App() {
   const center = [51.05454418294424, 13.744604518424293]
-
+  const spots = [
+    {
+      latlng: [51.06385440680908, 13.747256519230437],
+      street:"Albert Platz, 01099 Dresden",
+      name: "Albi"
+    },
+    {
+        latlng: [51.06710519422941, 13.736075954254634],
+        street:"Eisenbahnstraße 4-2, 01097 Dresden",
+        name: "DIY"
+      }]
 
   
   return (
@@ -17,12 +27,13 @@ function App() {
         url='https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=tbObNRj6Au49couK6i4U'
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     />
-      <Marker position={[51.05,13.74]}>
+    {spots.map(({latlng, street, name }) =>
+      <Marker position={latlng}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+          {name} <br /> {street}
         </Popup>
       </Marker>
-
+      )}
     </MapContainer>
   )
 }
